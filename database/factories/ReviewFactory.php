07df83163ffb8fda67a83b82d6c282db8d3ca9b8
Vinfante06 <vdinfanteg@gmail.com>
2026-09-15 16:53:28 -0500

    Factories and pint method

--- database/factories/ReviewFactory.php
+++ database/factories/ReviewFactory.php
@@ -8,17 +8,28 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 /**
  * @extends Factory<Review>
  */
-class ReviewFactory extends Factory
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
+    protected $model = Product::class;
+
     public function definition(): array
     {
         return [
-            //
+            'title' => $this->faker->words(3, true),
+            'description' => $this->faker->paragraph(),
+            'price' => $this->faker->randomFloat(2, 5, 300),
+            'stock' => $this->faker->numberBetween(0, 100),
+            'format' => $this->faker->randomElement(['Digital', 'Físico']),
+            'type' => $this->faker->randomElement(['Juego', 'DLC', 'Accesorio']),
+            'platform' => $this->faker->randomElement(['PC', 'PS5', 'Xbox', 'Switch']),
+            'category_id' => Category::factory(),
         ];
     }
 }
