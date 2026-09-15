51c614ffc47924c6d11ee83ebc4b0a608ec41cd4
bsgonzalem <bsgonzalem@eafit.edu.co>
2026-09-15 18:01:54 -0500

    Implement Item model and service

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -3,18 +3,28 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Item extends Model
 {
     /**
-     * Item ATTRIBUTES
+     * ITEM ATTRIBUTES
+     *
      * $this->attributes['id'] - int - contains the item primary key (id)
      * $this->attributes['quantity'] - int - contains the item quantity
      * $this->attributes['unitPrice'] - double - contains the item unit price
      * $this->attributes['subtotal'] - double - contains the item subtotal
+     * $this->attributes['order_id'] - int - contains the order foreign key
+     * $this->attributes['product_id'] - int - contains the product foreign key
      */
 
-    protected $fillable = ['quantity', 'unitPrice', 'subtotal'];
+    protected $fillable = [
+        'quantity',
+        'unitPrice',
+        'subtotal',
+        'order_id',
+        'product_id',
+    ];
 
     public function getId(): int
     {
@@ -54,5 +64,40 @@ class Item extends Model
     public function setSubtotal(float $subtotal): void
     {
         $this->attributes['subtotal'] = $subtotal;
+    }
+
+    public function order(): BelongsTo
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function getOrder(): Order
+    {
+        return $this->order;
+    }
+
+    public function setOrder(Order $order): void
+    {
+        $this->order()->associate($order);
+    }
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function getProduct(): Product
+    {
+        return $this->product;
+    }
+
+    public function setProduct(Product $product): void
+    {
+        $this->product()->associate($product);
+    }
+
+    public function calculateSubtotal(): float
+    {
+        return $this->getQuantity() * $this->getUnitPrice();
     }
 }
