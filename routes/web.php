b08eba309c6ca2512d146a1b98edae259f6c69ab
Vinfante06 <vdinfanteg@gmail.com>
2026-09-15 23:49:37 -0500

    routes fixing

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,10 @@
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::resource('products', ProductController::class)->except(['create', 'edit']);
+Route::get('/product', 'App\Http\Controllers\ProductController@index')->name('product.index');
+Route::get('/product/create', 'App\Http\Controllers\ProductController@create')->name('product.create');
+Route::post('/product', 'App\Http\Controllers\ProductController@save')->name('product.save');
+Route::get('/product/{id}', 'App\Http\Controllers\ProductController@show')->name('product.show');
+Route::get('/product/{id}/edit', 'App\Http\Controllers\ProductController@edit')->name('product.edit');
+Route::put('/product/{id}', 'App\Http\Controllers\ProductController@update')->name('product.update');
+Route::delete('/product/{id}', 'App\Http\Controllers\ProductController@delete')->name('product.delete');
