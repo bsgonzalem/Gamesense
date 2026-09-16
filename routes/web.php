416353176a4a8a0f61231eb7604e4a103087be30
Vinfante06 <vdinfanteg@gmail.com>
2026-09-15 23:56:23 -0500

    routes fixing

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
-use App\Http\Controllers\ReviewController;
+use App\Http\Controllers\ReviewtController;
 use Illuminate\Support\Facades\Route;
 
-Route::Resource('reviews', ReviewController::class);
+Route::get('/review', 'App\Http\Controllers\ReviewController@index')->name('review.index');
+Route::get('/review/create', 'App\Http\Controllers\ReviewController@create')->name('review.create');
+Route::post('/review', 'App\Http\Controllers\ReviewController@save')->name('review.save');
+Route::get('/review/{id}', 'App\Http\Controllers\ReviewController@show')->name('review.show');
+Route::get('/review/{id}/edit', 'App\Http\Controllers\ReviewController@edit')->name('review.edit');
+Route::put('/review/{id}', 'App\Http\Controllers\ReviewController@update')->name('review.update');
+Route::delete('/review/{id}', 'App\Http\Controllers\ReviewController@delete')->name('review.delete');
