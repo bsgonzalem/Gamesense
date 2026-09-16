e82a8b6d52da96b7ab73efcb3454105146d96c7e
bsgonzalem <bsgonzalem@eafit.edu.co>
2026-09-15 23:33:42 -0500

    Routes

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ReviewController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
+Auth::routes();
 
 Route::get('/category', 'App\Http\Controllers\CategoryController@index')->name('category.index');
 Route::get('/category/create', 'App\Http\Controllers\CategoryController@create')->name('category.create');
@@ -12,6 +13,6 @@ Route::get('/category/{id}', 'App\Http\Controllers\CategoryController@show')->na
 Route::get('/category/{id}/edit', 'App\Http\Controllers\CategoryController@edit')->name('category.edit');
 Route::put('/category/{id}', 'App\Http\Controllers\CategoryController@update')->name('category.update');
 Route::delete('/category/{id}', 'App\Http\Controllers\CategoryController@delete')->name('category.delete');
+
 Route::resource('users', UserController::class);
 Route::resource('reviews', ReviewController::class);
-Auth::routes();
